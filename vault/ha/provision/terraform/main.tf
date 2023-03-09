e347459dc7b02e3382b019ca6a5d6fe443f1f70f
Chen Shang-Kuei <c.shangkuei@gmail.com>
2023-03-09 21:06:28 +0800

    fix: fix vault-ha terraform stack

--- vault/ha/provision/terraform/main.tf
+++ vault/ha/provision/terraform/main.tf
@@ -4,7 +4,10 @@ provider "vault" {
 }
 resource "vault_auth_backend" "kubernetes" {
   type = "kubernetes"
+}
 
+resource "vault_kubernetes_auth_backend_config" "example" {
+  backend                = vault_auth_backend.kubernetes.path
   kubernetes_host        = var.kubernetes_host
-  disable_iss_validation = true
+  disable_iss_validation = "true"
 }
