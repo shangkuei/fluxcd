893c65912d47e27857095d8e0135bd754a888278
Chen Shang-Kuei <c.shangkuei@gmail.com>
2023-03-10 08:44:37 +0800

    fix: skip terraform vault provider v3.13.0

--- vault/ha/provision/terraform/versions.tf
+++ vault/ha/provision/terraform/versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = ">= 1.3"
   required_providers {
     vault = {
-      source = "hashicorp/vault"
+      source  = "hashicorp/vault"
+      version = ">= 3, != 3.13.0"
     }
   }
 }
