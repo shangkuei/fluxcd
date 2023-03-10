c8a440fe0206046e044934581697331114cf35eb
Chen Shang-Kuei <c.shangkuei@gmail.com>
2023-03-10 20:51:35 +0800

    fix: media-plex-meta-manager kustomization stack

--- media/plex-meta-manager/credential/terraform/main.tf
+++ media/plex-meta-manager/credential/terraform/main.tf
@@ -16,50 +16,43 @@ provider "vault" {
 }
 
 resource "vault_kv_secret_v2" "notifiarr" {
-  mount               = var.secret_path
-  name                = "notifiarr"
-  delete_all_versions = true
-  data_json           = jsonencode(var.notifiarr)
+  mount     = var.secret_path
+  name      = "notifiarr"
+  data_json = jsonencode(var.notifiarr)
 }
 
 resource "vault_kv_secret_v2" "mdb" {
-  mount               = var.secret_path
-  name                = "mdb"
-  delete_all_versions = true
-  data_json           = jsonencode(var.mdb)
+  mount     = var.secret_path
+  name      = "mdb"
+  data_json = jsonencode(var.mdb)
 }
 
 resource "vault_kv_secret_v2" "omdb" {
-  mount               = var.secret_path
-  name                = "omdb"
-  delete_all_versions = true
-  data_json           = jsonencode(var.omdb)
+  mount     = var.secret_path
+  name      = "omdb"
+  data_json = jsonencode(var.omdb)
 }
 
 resource "vault_kv_secret_v2" "tmdb" {
-  mount               = var.secret_path
-  name                = "tmdb"
-  delete_all_versions = true
-  data_json           = jsonencode(var.tmdb)
+  mount     = var.secret_path
+  name      = "tmdb"
+  data_json = jsonencode(var.tmdb)
 }
 
 resource "vault_kv_secret_v2" "plex" {
-  mount               = var.secret_path
-  name                = "plex"
-  delete_all_versions = true
-  data_json           = jsonencode(var.plex)
+  mount     = var.secret_path
+  name      = "plex"
+  data_json = jsonencode(var.plex)
 }
 
 resource "vault_kv_secret_v2" "radarr" {
-  mount               = var.secret_path
-  name                = "radarr"
-  delete_all_versions = true
-  data_json           = jsonencode(var.radarr)
+  mount     = var.secret_path
+  name      = "radarr"
+  data_json = jsonencode(var.radarr)
 }
 
 resource "vault_kv_secret_v2" "sonarr" {
-  mount               = var.secret_path
-  name                = "sonarr"
-  delete_all_versions = true
-  data_json           = jsonencode(var.sonarr)
+  mount     = var.secret_path
+  name      = "sonarr"
+  data_json = jsonencode(var.sonarr)
 }
--- vault/ha/provision/terraform/namespace_media.tf
+++ vault/ha/provision/terraform/namespace_media.tf
@@ -2,6 +2,7 @@ locals {
   namespace_media = {
     name   = "media"
     runner = "tf-runner"
+    pmm    = "plex-meta-manager"
   }
 }
 
@@ -58,6 +59,53 @@ path "${vault_mount.secret_media.path}/destroy/*" {
 
 path "${vault_mount.secret_media.path}/metadata/*" {
   capabilities = [ "create", "read", "update", "list", "delete" ]
+}
+EOT
+}
+
+resource "vault_kubernetes_auth_backend_role" "media_plex_meta_manager" {
+  depends_on = [
+    vault_policy.media_plex_meta_manager,
+  ]
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "${local.namespace_media.name}-${local.namespace_media.pmm}"
+  bound_service_account_names      = [local.namespace_media.pmm]
+  bound_service_account_namespaces = [local.namespace_media.name]
+  token_policies                   = [vault_policy.media_plex_meta_manager.name]
+  token_bound_cidrs                = [var.kubernetes_cluster_cidrs]
+}
+
+resource "vault_policy" "media_plex_meta_manager" {
+  name = "${local.namespace_media.name}-${local.namespace_media.pmm}"
+
+  policy = <<EOT
+# read notifiarr secrets in key-value secrets engine
+path "${vault_mount.secret_media.path}/data/notifiarr" {
+  capabilities = [ "read" ]
+}
+# read mdb secrets in key-value secrets engine
+path "${vault_mount.secret_media.path}/data/mdb" {
+  capabilities = [ "read" ]
+}
+# read omdb secrets in key-value secrets engine
+path "${vault_mount.secret_media.path}/data/omdb" {
+  capabilities = [ "read" ]
+}
+# read tmdb secrets in key-value secrets engine
+path "${vault_mount.secret_media.path}/data/tmdb" {
+  capabilities = [ "read" ]
+}
+# read plex secrets in key-value secrets engine
+path "${vault_mount.secret_media.path}/data/plex" {
+  capabilities = [ "read" ]
+}
+# read radarr secrets in key-value secrets engine
+path "${vault_mount.secret_media.path}/data/radarr" {
+  capabilities = [ "read" ]
+}
+# read sonarr secrets in key-value secrets engine
+path "${vault_mount.secret_media.path}/data/sonarr" {
+  capabilities = [ "read" ]
 }
 EOT
 }
